4a675dba7958f066051e22fb9f4df88750d22d99
Anaïs RV <anaisrv@gmail.com>
2024-12-18 10:31:19 +0100

    test(KPT_4): failing test - Supervivientes creation

---
+++ zombies/tests/test_superviviente.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from zombies.models import Superviviente
+
+class SupervivienteTestCase(TestCase):
+    def test_crear_superviviente(self):
+
+        """ Test para crear un superviviente e inicializarlo correctamente """
+
+        # Creamos un superviviente con un nombre
+        superviviente = Superviviente(nombre="Arancha")
+
+        # Verificamos valores iniciales
+        self.assertEqual(superviviente.nombre, "Arancha")
+        self.assertEqual(superviviente.heridas, 0)
+        self.assertEqual(superviviente.acciones, 3)
+        self.assertEqual(superviviente.experiencia, 0)
+        self.assertEqual(superviviente.nivel, "Azul")
