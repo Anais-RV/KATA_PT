5fa0dbfc062de0626cb81af2330740899d5abdf2
Anaïs RV <anaisrv@gmail.com>
2024-12-23 21:02:26 +0100

    fix(KPT-26): handle ValidationError - EquipoView POST

--- zombies/views/equipo.py
+++ zombies/views/equipo.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +14,11 @@ class EquipoView(APIView):
     def post(self, request):
         serializer = EquipoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except ValidationError as e:
+                # Capturamos ValidationError y devolvemos un código 400 con el mensaje de error
+                return Response({"error": e.messages}, status=status.HTTP_400_BAD_REQUEST)
+        # Si el serializer no es válido
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
