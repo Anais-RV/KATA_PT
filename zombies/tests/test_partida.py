f52fab4df0b6c27aa7077449afdeb699c87ef9a7
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-01-02 21:32:32 +0100

    test(KPT-34): refactor test - failing test - add initial empty game

--- zombies/tests/test_partida.py
+++ zombies/tests/test_partida.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from zombies.models import Partida, Jugador, Zombi, JugadorPartidaº
+from zombies.models import Partida
 
 class PartidaTestCase(TestCase):
     def test_partida_comienza_sin_supervivientes(self):
-        partida = Partida()
+        """Test para verificar que una partida comienza con 0 supervivientes."""
+        partida = Partida.objects.create(jugador="Jugador 1")  
         self.assertEqual(partida.supervivientes.count(), 0)
-        return super().partida_comienza_sin_supervivientes()
