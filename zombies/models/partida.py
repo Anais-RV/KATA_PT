ca8834cdc6469ed3079640055663980214310d6a
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-01-02 21:40:48 +0100

    test(KPT-35): Partida model - pass initial empty survivors test

--- zombies/models/partida.py
+++ zombies/models/partida.py
@@ -1,7 +1,7 @@
-# from django.db import models
+from django.db import models
 
-# class Partida(models.Model):
-#     nombre = models.CharField(max_length=100, unique=True)
-#     supervivientes = models.ManyToManyField('Superviviente', related_name='partidas', blank=True)
-#     def __str__(self):
-#         return self.nombre
+class Partida(models.Model):
+    jugador = models.CharField(max_length=100, unique=True)
+    supervivientes = models.ManyToManyField('Superviviente', related_name='partidas', blank=True)
+    def __str__(self):
+        return self.jugador
