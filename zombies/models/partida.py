1ca369fcc233560e6f4139e1a8521f9ad935ca6c
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-01-02 21:52:40 +0100

    feat(KPT-36): pass test for adding survivors to a game

--- zombies/models/partida.py
+++ zombies/models/partida.py
@@ -3,5 +3,6 @@ from django.db import models
 class Partida(models.Model):
     jugador = models.CharField(max_length=100, unique=True)
     supervivientes = models.ManyToManyField('Superviviente', related_name='partidas', blank=True)
+
     def __str__(self):
         return self.jugador
--- zombies/tests/test_partida.py
+++ zombies/tests/test_partida.py
@@ -6,3 +6,19 @@ class PartidaTestCase(TestCase):
         """Test para verificar que una partida comienza con 0 supervivientes."""
         partida = Partida.objects.create(jugador="Jugador 1")  
         self.assertEqual(partida.supervivientes.count(), 0)
+
+    def test_añadir_supervivientes_a_partida(self):
+        """Test para verificar que una partida puede tener supervivientes."""
+        partida = Partida.objects.create(jugador="Jugador 1")
+        
+        # Crear supervivientes
+        superviviente1 = partida.supervivientes.create(nombre="Superviviente 1")
+        superviviente2 = partida.supervivientes.create(nombre="Superviviente 2")
+        
+        # Añadir dos supervivientes a la partida
+        partida.supervivientes.add(superviviente1, superviviente2)
+        
+        # Verificar que la partida tiene dos supervivientes
+        self.assertEqual(partida.supervivientes.count(), 2)
+        
+    
