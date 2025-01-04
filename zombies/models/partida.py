5e1d91fc3502a9cc3f8bbba6262a03890aad6f1b
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-01-04 21:00:18 +0100

    feat(KPT-40): refact - implement signals for unique survivor names validation in games

--- zombies/models/partida.py
+++ zombies/models/partida.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.core.exceptions import ValidationError
+from django.dispatch import receiver
 
 class Partida(models.Model):
     jugador = models.CharField(max_length=100, unique=True)
@@ -6,3 +9,11 @@ class Partida(models.Model):
 
     def __str__(self):
         return self.jugador
+
+
+@receiver(m2m_changed, sender=Partida.supervivientes.through) # disparar la señal m2m_changed cada vez que se modifique la relación de muchos a muchos entre Partida y Superviviente
+def validar_supervivientes_unicos(sender, instance, action, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:
+        nombres = [superviviente.nombre for superviviente in instance.supervivientes.all()]
+        if len(nombres) != len(set(nombres)):
+            raise ValidationError("Los nombres de los supervivientes deben ser únicos dentro de la partida.")
--- zombies/tests/test_partida.py
+++ zombies/tests/test_partida.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from zombies.models import Partida
+from zombies.models import Partida, Superviviente
 
 class PartidaTestCase(TestCase):
     def test_partida_comienza_sin_supervivientes(self):
@@ -13,8 +13,8 @@ class PartidaTestCase(TestCase):
         partida = Partida.objects.create(jugador="Jugador 1")
         
         # Crear supervivientes
-        superviviente1 = partida.supervivientes.create(nombre="Superviviente 1")
-        superviviente2 = partida.supervivientes.create(nombre="Superviviente 2")
+        superviviente1 = Superviviente.objects.create(nombre="Superviviente 1")
+        superviviente2 = Superviviente.objects.create(nombre="Superviviente 2")
         
         # Añadir dos supervivientes a la partida
         partida.supervivientes.add(superviviente1, superviviente2)
@@ -29,8 +29,10 @@ class PartidaTestCase(TestCase):
         partida = Partida.objects.create(jugador="Jugador 1")
         
         # Crear un superviviente
-        superviviente1 = partida.supervivientes.create(nombre="Superviviente 1")
+        superviviente1 = Superviviente.objects.create(nombre="Superviviente 1")
+        partida.supervivientes.add(superviviente1)
         
         # Intentar crear otro superviviente con el mismo nombre
+        superviviente2 = Superviviente.objects.create(nombre="Superviviente 1")
         with self.assertRaises(ValidationError):
-            partida.supervivientes.create(nombre="Superviviente 1")
+            partida.supervivientes.add(superviviente2)
