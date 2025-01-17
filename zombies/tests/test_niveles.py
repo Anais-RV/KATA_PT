795b7fdf935bcd7b6236993078cb80833c69e37b
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-01-17 20:09:17 +0100

    test(KPT-55) failling test - game level updated

--- zombies/tests/test_niveles.py
+++ zombies/tests/test_niveles.py
@@ -21,3 +21,19 @@ class Niveles(TestCase):
         superviviente.experiencia = 43
         superviviente.save()
         self.assertEqual(superviviente.nivel, "Rojo")   
+    
+class PartidaNivelTestCase(TestCase):
+    def test_actualizar_nivel_partida(self):
+        """Test para verificar que el nivel de la partida se actualiza según los supervivientes vivos."""
+        partida = Partida.objects.create(jugador="Jugador 1")
+        sup1 = Superviviente.objects.create(nombre="Superviviente 1", nivel='Azul', vivo=True)
+        sup2 = Superviviente.objects.create(nombre="Superviviente 2", nivel='Naranja', vivo=True)
+        partida.supervivientes.add(sup1, sup2)
+
+        partida.actualizar_nivel()
+        self.assertEqual(partida.nivel, 'Naranja')
+
+        sup2.vivo = False
+        sup2.save()
+        partida.actualizar_nivel()
+        self.assertEqual(partida.nivel, 'Azul')
